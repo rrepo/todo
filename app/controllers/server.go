b19ea7ee2f06e7431d3a437dc4a16734a8fe874c
rrepo <reporepo254@gmail.com>
2021-09-25 18:33:22 +0900

    13

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -65,7 +65,6 @@ func StartMainServer() error {
 	files:= http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/",files))
 
-	http.HandleFunc("/favicon.ico",faviconHandler)
 	http.HandleFunc("/", top)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"heroku_todo/app/controllers"
 	"heroku_todo/app/models"
 	_ "heroku_todo/config"
-	_"log"
 	_ "log"
+	"net/http"
 	_ "os/user"
 )
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/favicon.ico")
+}
 
 func main() {
 	fmt.Println(models.Db)
 	controllers.StartMainServer()
+	http.HandleFunc("/favicon.ico",faviconHandler)
 }
